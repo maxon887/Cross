ef7123f4f9f99337431cd1aac06e639fa961f533
maxon887 <mouse@iMax.local>
2015-05-07 12:57:51 +0300

    Cleaning

---
+++ Games/Jim the Snake/Platforms/iOS/Jim the Snake/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jim the Snake
+//
+//  Created by mouse on 5/7/15.
+//  Copyright (c) 2015 Lukyanau Maksim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
--- Games/Snaky/Platform/Windows/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Cross.h"
-#include "SnakyGame.h"
-
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE instancePrev, LPSTR args, int winShow){
-	LauncherWIN* launcher = new LauncherWIN();
-	SnakyGame* game = new SnakyGame(launcher);
-	return CrossMain(game, instance, winShow);
-}
