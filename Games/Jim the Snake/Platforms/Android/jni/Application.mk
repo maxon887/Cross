d0042e2dcd450e542eab9543e17d9e5d8a0bfadf
maxon887 <maxon887@gmail.com>
2016-01-07 18:12:25 +0200

    Removing Jim the Snake from repository

--- Games/Jim the Snake/Platforms/Android/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-NDK_TOOLCHAIN_VERSION := 4.8
-
-APP_CPPFLAGS += -std=c++11
-APP_STL := c++_shared
-
-APP_PLATFORM := android-9
-APP_ABI := armeabi armeabi-v7a x86
--- Games/Jim the Snake/Platforms/Windows/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Cross.h"
-#include "JimTheSnake.h"
-#include "LauncherWIN.h"
-
-Game* CrossMain(Launcher* launcher){
-	((LauncherWIN*)launcher)->SetTargetSize(540, 960);
-	return new JimTheSnake(launcher);
-}
