0e935f02c80d3819b6d60fddcbcf9905209f16ec
m_lukyanov <m_lukyanov@wargaming.net>
2017-04-29 17:25:04 +0300

    PhysicsScreen added

---
+++ Games/Demo/Sources/Physics/PhysicsScreen.cpp
@@ -0,0 +1,97 @@
+/*	Copyright © 2015 Lukyanau Maksim
+
+	This file is part of Cross++ Game Engine.
+
+    Cross++ Game Engine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cross++ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cross++.  If not, see <http://www.gnu.org/licenses/>			*/
+#include "PhysicsScreen.h"
+#include "Entity.h"
+#include "GraphicsGL.h"
+#include "Graphics3D.h"
+#include "Shaders/LightShader.h"
+#include "Material.h"
+#include "Light.h"
+#include "Camera.h"
+#include "Shaders/MultiLightShader.h"
+#include "Texture.h"
+#include "Graphics2D.h"
+
+void PhysicsScreen::Start() {
+	CameraControlsScene::Start();
+	GetCamera()->SetPosition(Vector3D(0.f, 3.f, -4.f));
+	GetCamera()->LookAt(Vector3D(0.f));
+	//light setups
+	Entity* light = new Entity();
+	light->AddComponent(new Light(Light::Type::POINT));
+	light->SetPosition(Vector3D(10.f, 7.f, -5.f));
+	AddEntity(light);
+	//*********************ROAD**********************
+	road_shader = (MultiLightShader*)gfxGL->GetShader(DefaultShader::MULTI_LIGHT);
+	road_shader->AddMakro("USE_DIFFUSE_MAP");
+	road_shader->AddMakro("USE_TILLING_FACTOR");
+	road_shader->AddProperty("Diffuse Texture", "uDiffuseTexture");
+	road_shader->AddProperty("Specular", "uSpecular", 0.5f);
+	road_shader->AddProperty("Shininess", "uShininess", 0.5f * 128.f);
+	road_shader->AddProperty("Tilling Factor", "uTillingFactor", 12.f);
+	road_shader->Compile();
+	road_diffuse = gfx2D->LoadTexture("gfx3D/RoadDiffuse.png");
+	road_diffuse->SetTilingMode(Texture::TilingMode::REPEAT);
+	road_mat = new Material(road_shader);
+	road_mat->SetPropertyValue("Diffuse Texture", road_diffuse);
+
+	Entity* road = gfx3D->LoadPrimitive(Graphics3D::Primitives::PLANE);
+	road->SetScale(250.f);
+	gfx3D->AdjustMaterial(road, road_mat, false);
+	AddEntity(road);
+
+	//*********************BALLS SHADER**********************
+	ball_shader = new LightShader("gfx3D/shaders/specular.vert", "gfx3D/shaders/specular.frag");
+	ball_shader->AddProperty("Diffuse Color", "uColor");
+	ball_shader->AddProperty("Specular Color", "uSpecularColor");
+	ball_shader->AddProperty("Shininess", "uShininess", 0.5f * 128.f);
+	ball_shader->Compile();
+	//*********************RED BALL**********************
+	red_mat = new Material(ball_shader);
+	red_mat->SetPropertyValue("Diffuse Color", Color::Red);
+	red_mat->SetPropertyValue("Specular Color", Color::White);
+
+
+	Entity* redBall = gfx3D->LoadPrimitive(Graphics3D::Primitives::SPHERE);
+	gfx3D->AdjustMaterial(redBall, red_mat);
+	redBall->SetPosition(Vector3D(-1.f, 1.f, 0.f));
+	AddEntity(redBall);
+	//*********************GREEN BALL**********************
+	green_mat = new Material(ball_shader);
+	green_mat->SetPropertyValue("Diffuse Color", Color::Green);
+	green_mat->SetPropertyValue("Specular Color", Color::White);
+
+	Entity* greenBall = gfx3D->LoadPrimitive(Graphics3D::Primitives::SPHERE);
+	gfx3D->AdjustMaterial(greenBall, green_mat);
+	greenBall->SetPosition(Vector3D(2.f, 1.f, 0.f));
+	AddEntity(greenBall);
+}
+
+void PhysicsScreen::Stop() {
+	delete green_mat;
+	delete red_mat;
+	delete ball_shader;
+	delete road_mat;
+	delete road_diffuse;
+	delete road_shader;
+	CameraControlsScene::Stop();
+}
+
+void PhysicsScreen::Update(float sec) {
+	CameraControlsScene::Update(sec);
+	//GetCamera()->LookAt(redBall->GetPosition());
+}
--- Games/Demo/Sources/Physics/PhysicsScreen.h
+++ Games/Demo/Sources/Physics/PhysicsScreen.h
@@ -15,47 +15,22 @@
     You should have received a copy of the GNU General Public License
     along with Cross++.  If not, see <http://www.gnu.org/licenses/>			*/
 #pragma once
-#include "Cross.h"
-#include "ScrollScreen.h"
-#include "Utils/Button.h"
-#include "Menu.h"
+#include "Graphics3D/CameraControlsScene.h"
+#include "Shaders/MultiLightShader.h"
 
-using namespace cross;
-
-class MainScreen : public ScrollScreen{
+class PhysicsScreen : public CameraControlsScene {
 public:
 	virtual void Start();
 	virtual void Stop();
 	virtual void Update(float sec);
 
 private:
-	Font* font;
-
-	Menu* current_menu;
-
-	Menu* main_menu;
-	Menu* graphics2D_menu;
-	Menu* graphics3D_menu;
-	Menu* physics_menu;
-
-	Menu* graphics3D_simple;
-	Menu* graphics3D_light;
-	Menu* graphics3D_maps;
-	Menu* graphics3D_misc;
-
-	void SetMenu(Menu* newMenu);
-
-	//connections
-    U64 resize_del;
-	void WindowResizedHandle(S32 width, S32 height);
-
-	//main menu
-	void OnGraphics2DClick();
-	void OnGraphics3DClick();
-	void OnPhysicsClick();
-	//graphics 3d menu
-	void OnSimpleClick();
-	void OnLightClick();
-	void OnMapsClick();
-	void OnMiscClick();
+	//road
+	MultiLightShader* road_shader;
+	Texture* road_diffuse;
+	Material* road_mat;
+	//balls
+	Shader* ball_shader;
+	Material* red_mat;
+	Material* green_mat;
 };
