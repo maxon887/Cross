2d580df72cb0f8cc88164283fd624611dbe92554
Max <maxon887@gmail.com>
2018-08-31 13:29:16 +0300

    String project adjustments

--- Tests/String/Main.cpp
+++ Tests/String/Main.cpp
@@ -1,10 +1,13 @@
-#include <stdio.h>
 #include "Cross.h"
 #include "Internals/MemoryManager.h"
+
+#include <stdio.h>
 
 using namespace cross;
 
 void main() {
+	String test = "This is a test string";
+
 	MemoryManager::Instance()->Dump();
 	getchar();
 }
